bfd4ff398af51577555979540732fd5a94a4fbfa
Syed Hassan Ali <syedhassanali2005@gmail.com>
2026-02-07 00:08:52 +0500

    updated todo logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inp, setInp] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  let setter = (e) => {
+    setInp(e.target.value);
+  };
+
+  let getter = () => {
+    if (inp.trim() === "") {
+      alert("Input cannot be empty");
+      return;
+    }
+    setTodo([...todo, inp]);
+    setInp("");
+  };
+
   return (
-    <div>
-      <h1>Hassan</h1>
+    <div style={{ display: "flex", height: "100vh", flexDirection: "column" }}>
+      <input type="text" value={inp} onChange={setter} />
+      <button onClick={getter}>Add</button>
+      <br />
+      <br />
+      <div
+        style={{
+          background: "red",
+          padding: 10,
+          flex: 1,
+          overflowY: "auto",
+        }}
+      >
+        <h1>The Todo List</h1>
+        <ul>
+          {todo.map((value, index) => (
+            <div key={index}>
+              <li>{value}</li>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
