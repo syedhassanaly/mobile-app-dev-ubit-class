05e1e786f34c82aab52d696dba3c32ca52d91969
Syed Hassan Ali <syedhassanali2005@gmail.com>
2026-02-12 22:53:07 +0500

    The program for input name and greet

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,28 @@ import React, { useState } from "react";
 
 function App() {
   const [inp, setInp] = useState("");
-  const [todo, setTodo] = useState([]);
+  let [name, setName] = useState("");
 
-  let setter = (e) => {
+  function settingInput(e) {
     setInp(e.target.value);
-  };
+  }
 
-  let getter = () => {
-    if (inp.trim() === "") {
-      alert("Input cannot be empty");
-      return;
-    }
-    setTodo([...todo, inp]);
+  function AddName() {
+    setName(inp);
     setInp("");
+  }
+
+  const greet = () => {
+    return "Hello " + name;
   };
 
   return (
-    <div style={{ display: "flex", height: "100vh", flexDirection: "column" }}>
-      <input type="text" value={inp} onChange={setter} />
-      <button onClick={getter}>Add</button>
-      <br />
-      <br />
-      <div
-        style={{
-          background: "red",
-          padding: 10,
-          flex: 1,
-          overflowY: "auto",
-        }}
-      >
-        <h1>The Todo List</h1>
-        <ul>
-          {todo.map((value, index) => (
-            <div key={index}>
-              <li>{value}</li>
-            </div>
-          ))}
-        </ul>
+    <div>
+      <div>
+        <h1>{greet()}</h1>
       </div>
+      <input type="text" value={inp} onChange={settingInput} />
+      <button onClick={AddName}>Set</button>
     </div>
   );
 }
